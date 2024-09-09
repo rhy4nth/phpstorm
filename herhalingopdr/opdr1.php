0e13220e2c1dfd8eedd18d7b008cc641c9452ab8
rhy4nth <9023396@student.zadkine.nl>
2024-09-09 14:02:01 +0200

    opdr1

---
+++ herhalingopdr/opdr1.php
@@ -0,0 +1,7 @@
+<?php
+$firstName= "Jan";
+$age = 25;
+$height =1.75;
+$isStudent=true;
+
+echo "I am $firstName $age, $height, $isStudent";
---
+++ les1/variable.php
@@ -0,0 +1,25 @@
+<?php
+//
+//$book= "Harry Potter";
+//$Book= "Niet correct";
+////ditIsEenVoorbeeld =
+//
+//$_SESSION["book"]= $book;
+//$age123
+//$echo
+//    echo $book;
+
+?>
+<?php
+$book= null;
+$anotherBook = null;
+
+$arrayOld =array('BLA', 'bla');
+$arrayOld->array_push($arrayOld, 'BLA'); //dit is oud
+
+$array =['BLA', 'bla'];  //dit is juist
+$int= 6;
+$float = 3.14;
+$bool = true;
+$bool = 0;
+
