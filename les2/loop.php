4bd49bf13ffc27dcec3935dbf64abc200428ee06
rhy4nth <9023396@student.zadkine.nl>
2024-09-20 12:47:51 +0200

    herhalingsopdrainjcAWD

---
+++ les2/loop.php
@@ -0,0 +1,22 @@
+<?php
+//$counter=0;
+//
+//for ($i = 1; $i <= 10; $i++) {
+//    $counter++;
+//    echo $counter;
+//}
+//
+//while ($counter < 10) {
+//    $counter++;
+//    echo $counter;
+//}
+//
+//do{
+//    $counter++;
+//    echo $counter;
+//}while($counter < 10);
+//
+//$arrayItems=[1,2,3,4,5,];
+//foreach($arrayItems as arrayitem){
+//    echo $arrayItem;
+//}
